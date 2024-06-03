e12fdf118fbc154cbdb2c00260a545f935f3bbae
Brodkrummen <arne.brot1@gmail.com>
2024-06-03 03:03:23 +0200

    Ending Screen Update

--- Assets/EndingScreen.cs
+++ Assets/EndingScreen.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EndingScreen : MonoBehaviour
 {
-    
+    InputActions InputActions;
+    private void Awake()
+    {
+        InputActions = new InputActions();
+        InputActions.Enable();
+    }
+    private void Start()
+    {
+        InputActions.Player.ExitGame.performed += ExitGame_performed;
+    }
+
+    private void ExitGame_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Application.Quit();
+    }
 }
